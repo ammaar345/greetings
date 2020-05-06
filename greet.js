53f2fa1a53e48b68c500f13ed8eb7aab2279e144
Ammaar <fakirammaar8@gmail.com>
2020-05-06 23:15:07 +0200

    bread

--- greet.js
+++ greet.js
@@ -4,13 +4,13 @@ const displayMsg=document.querySelector(".msg");
 const iCount=document.querySelector(".counter");
 let greet=Greet();
 function displayClick(){
-        
+
     const radLang=document.querySelector("input[name='languageType']:checked");
 displayMsg.innerHTML="";
     var itemVal=radLang.value;
    // alert(greet.languageChoice(itemVal))
  displayMsg.innerHTML= greet.languageChoice(itemVal) ;
- 
+     
  iCount.innerHTML=greet.inc();
 name.value='';
 }
