66e5b62b7502fc52589f91f0d5415d5e46194cea
Ammaar <fakirammaar8@gmail.com>
2020-05-13 15:26:33 +0200

    .

--- greet.js
+++ greet.js
@@ -1,20 +1,21 @@
-let greet=Greet();
-const userN=document.querySelector(".nameOfUser");
-const btnDisplay=document.querySelector(".btnDisplay");
-const displayMsg=document.querySelector(".msg");
-const iCount=document.querySelector(".counter");
+let greet = Greet();
+const userN = document.querySelector(".nameOfUser");
+const btnDisplay = document.querySelector(".btnDisplay");
+const displayMsg = document.querySelector(".msg");
+const iCount = document.querySelector(".counter");
 
 
 
-function displayClick(){
-  const radLang=document.querySelector("input[name='languageType']:checked");
-displayMsg.innerHTML=""; 
-var itemVal=radLang.value;
-var name=userN.value;
-   // alert(greet.languageChoice(itemVal))
- displayMsg.innerHTML= greet.languageChoice(itemVal) ;
- iCount.innerHTML=greet.greetCounter(name)
-//alert(itemVal)
- name.value='';
+function displayClick() {
+  const radLang = document.querySelector("input[name='languageType']:checked");
+  displayMsg.innerHTML = "";
+  var itemVal = radLang.value;
+  var name = userN.value;
+  // alert(greet.languageChoice(itemVal))
+  displayMsg.innerHTML = greet.languageChoice(itemVal,name);
+  iCount.innerHTML =greet.greetCounter(name);
+  //alert(itemVal)
+  name.value = '';
+  (greet.nameStorage());
 }
-btnDisplay.addEventListener("click",displayClick);
+btnDisplay.addEventListener("click", displayClick);
--- greetFactory.js
+++ greetFactory.js
@@ -1,60 +1,69 @@
-function Greet (){
+function Greet() {
     var msg;
-   // let selectedValue;// if it doesnt work , move it back under the radio button declaration
-// add a global variable
-var namesGreeted = {};
-var greetingsCounter=0;
-function greetCounter(userName){
-   //when the greet button is pressed check if this user was already greeted before
-//by looking if the userName exists in namesGreeted if not increment this counter and update the screen
-if (namesGreeted[userName] === undefined){
-    greetingsCounter++;
-    //add an entry for the user that was greeted in the Object Map
-    namesGreeted[userName] = 0;
-    //update the DOM to display the counter
-     
-}
-return greetingsCounter;
-}
+    // let selectedValue;// if it doesnt work , move it back under the radio button declaration
+    // add a global variable
+    var namesGreeted = {};
+    var greetingsCounter = 0;
+    function greetCounter(userName) {
+        //when the greet button is pressed check if this user was already greeted before
+        //by looking if the userName exists in namesGreeted if not increment this counter and update the screen
+        if (namesGreeted[userName] === undefined) {
+            greetingsCounter++;
+            //add an entry for the user that was greeted in the Object Map
+            namesGreeted["Name"]=userName ;
+            //update the DOM to display the counter
 
-// function checked(){
-//     const radLang=document.querySelectorAll("input[name='languageType']");
+        }
+       return greetingsCounter;
+    }
 
-    
-//             for (const rb of radLang) {
-//                 if (rb.checked) {
-//                     selectedValue = rb.value;
-//                     break;
-//                 }
-//             }
-// return selectedValue;
-//         };
+    function counter(){
+        return greetingsCounter;
+    }
 
-    function languageChoice(lang){
-        var name=userN.value;
-            
-      if (lang==="English"){
-        msg="Hello, "+name;
-       
-    }
-        else if (lang==="Afrikaans"){
-    msg="Halo, "+name;
-    
-    }
-    
-    else if(lang==="Xhosa")
-    {
-    msg="Molo ,"+name;
-   
-    } 
-    
-    
-    return msg
-    }
-    return {languageChoice,
-greetCounter,
-//checked
+function nameStorage(){
+    console.log(namesGreeted)
+    return namesGreeted;
 
 
 }
+    // function checked(){
+    //     const radLang=document.querySelectorAll("input[name='languageType']");
+
+
+    //             for (const rb of radLang) {
+    //                 if (rb.checked) {
+    //                     selectedValue = rb.value;
+    //                     break;
+    //                 }
+    //             }
+    // return selectedValue;
+    //         };
+
+    function languageChoice(lang,name) {
+        
+        if (lang === "English") {
+            msg = "Hello, " + name;
+
+        }
+        else if (lang === "Afrikaans") {
+            msg = "Halo, " + name;
+
+        }
+
+        else if (lang === "Xhosa") {
+            msg = "Molo ," + name;
+
+        }
+
+
+        return msg
+    }
+    return {
+        languageChoice,
+        greetCounter,
+        nameStorage,
+        counter
+
+    }
 }
