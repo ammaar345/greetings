035fe00c1f587d86080452421e08b6b272e24a11
Ammaar <fakirammaar8@gmail.com>
2020-05-14 13:05:04 +0200

    added local storage functionality with name+count

--- greet.js
+++ greet.js
@@ -1,21 +1,33 @@
-let greet = Greet();
 const userN = document.querySelector(".nameOfUser");
 const btnDisplay = document.querySelector(".btnDisplay");
 const displayMsg = document.querySelector(".msg");
 const iCount = document.querySelector(".counter");
+const storedNameState = localStorage["Names"];
 
+let nameState;
+if (localStorage["Names"]) {
+  nameState =JSON.parse(localStorage['Names'])
+   
 
+}
 
+  const greet = Greet(nameState)  ;
+  iCount.innerHTML = greet.countNamesStorage();
 function displayClick() {
+  var name=userN.value;
   const radLang = document.querySelector("input[name='languageType']:checked");
   displayMsg.innerHTML = "";
   var itemVal = radLang.value;
-  var name = userN.value;
   // alert(greet.languageChoice(itemVal))
-  displayMsg.innerHTML = greet.languageChoice(itemVal,name);
-  iCount.innerHTML =greet.greetCounter(name);
-  //alert(itemVal)
+  displayMsg.innerHTML = greet.languageChoice(itemVal, name);
+  greet.greetCounter(name);
+  iCount.innerHTML = greet.countNamesStorage();
+   
+ var storingNames=greet.nameStorage();
+ var nameStrings =  JSON.stringify(storingNames);
+localStorage['Names'] = nameStrings;
+//alert(greet.greetCounter(name))
   name.value = '';
-  (greet.nameStorage());
+//  (greet.nameStorage());
 }
 btnDisplay.addEventListener("click", displayClick);
--- greetFactory.js
+++ greetFactory.js
@@ -1,28 +1,38 @@
-function Greet() {
+function Greet(initialState) {
     var msg;
     // let selectedValue;// if it doesnt work , move it back under the radio button declaration
     // add a global variable
-    var namesGreeted = {};
-    var greetingsCounter = 0;
+    var namesGreeted = initialState ? initialState :{};
+   // var greetingsCounter = 0;
     function greetCounter(userName) {
         //when the greet button is pressed check if this user was already greeted before
         //by looking if the userName exists in namesGreeted if not increment this counter and update the screen
         if (namesGreeted[userName] === undefined) {
-            greetingsCounter++;
+           // greetingsCounter++;
             //add an entry for the user that was greeted in the Object Map
-            namesGreeted["Name"]=userName ;
+            namesGreeted[userName]=1 ;
             //update the DOM to display the counter
 
         }
-       return greetingsCounter;
+//        else if(namesGreeted[userName]===0){
+// namesGreeted[userName]=1
+//         } 
+//         else if (namesGreeted[userName]=1){
+//             greetingsCounter=  greetingsCounter;
+//             msg="This name has already been added."
+//             return msg
+//         }
+        
+    //    return greetingsCounter;
     }
 
-    function counter(){
-        return greetingsCounter;
-    }
-
+   
+function countNamesStorage(){
+var namesCount=Object.keys(namesGreeted)
+return namesCount.length;
+}
 function nameStorage(){
-    console.log(namesGreeted)
+    console.log(namesGreeted);
     return namesGreeted;
 
 
@@ -63,7 +73,8 @@ function nameStorage(){
         languageChoice,
         greetCounter,
         nameStorage,
-        counter
+     
+        countNamesStorage
 
     }
 }
